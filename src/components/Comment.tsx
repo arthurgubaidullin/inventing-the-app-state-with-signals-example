c6b66caa4c9fe93d03d261bc070cc73107220503
Artur Gubaidullin <lapteuh@gmail.com>
2025-03-15 10:15:23 +0000

    Fix component

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,12 +3,18 @@ import { EditCommentForm } from "./EditCommentForm";
 
 export const Comment = ({ comment }: { comment: CommentState }) => {
   return (
-    <div class="grid gap-4">
-      <h5 class="">Comment</h5>
+    <div class="grid gap-4 grid-cols-2">
+      <div class="flex flex-col gap-4">
+        <h5 class="">Comment</h5>
 
-      <p>{comment.data.value.content}</p>
+        <div class="flex-1">
+          <p>{comment.data.value.content}</p>
+        </div>
+      </div>
 
-      <EditCommentForm comment={comment} />
+      <div>
+        <EditCommentForm comment={comment} />
+      </div>
     </div>
   );
 };
